39e4c68cac3fcbb094e7d44c2d5b24a44974a972
Caio Almeida <116771074+k4neca@users.noreply.github.com>
2025-03-22 20:44:47 -0300

    Add files via upload

---
+++ fipecontroller/src/main/java/fipecontroller/fipecontroller/FipecontrollerApplication.java
@@ -0,0 +1,13 @@
+package fipecontroller.fipecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipecontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipecontrollerApplication.class, args);
+	}
+
+}
---
+++ fipecontroller/src/test/java/fipecontroller/fipecontroller/FipecontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package fipecontroller.fipecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipecontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
