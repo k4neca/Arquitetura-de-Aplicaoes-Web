7481049e7d28fabdfe6b0189ededee0c0904283c
Caio Almeida <116771074+k4neca@users.noreply.github.com>
2025-04-24 22:52:34 -0300

    Delete Av1 directory

--- Av1/src/main/java/CaioAlmeida/Av1/Repositories/ColaboradorRepository.java
+++
@@ -1,7 +0,0 @@
-package CaioAlmeida.Av1.Repositories;
-
-import CaioAlmeida.Av1.Models.Colaborador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> {
-}
--- Av1/src/main/java/CaioAlmeida/Av1/Repositories/EmpresaRepository.java
+++
@@ -1,7 +0,0 @@
-package CaioAlmeida.Av1.Repositories;
-
-import CaioAlmeida.Av1.Models.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-}
